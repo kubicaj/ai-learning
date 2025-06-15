98d5471633c463e067480499d8b5bc9a69a7150a
Juraj Kubica <juraj.kubica@profinit.eu>
2025-06-15 22:21:48 +0200

    Add AI assistent for learning and deploy it

--- src/langgraph/simple_chat_with_tool/app.py
+++ src/langgraph/simple_chat_with_tool/app.py
@@ -1,18 +1,50 @@
 import uuid
-import json
 import gradio as gr
 
 from typing import Annotated
 
+from langgraph.prebuilt import ToolNode, tools_condition
+from pydantic import BaseModel
 from IPython.display import Image, display
+from dotenv import load_dotenv
 from langgraph.constants import START, END
 from langgraph.graph import StateGraph
-from pydantic import BaseModel
-
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import MemorySaver
+from langchain.agents import Tool
+from langchain_community.utilities import GoogleSerperAPIWrapper
 from src.langgraph.llm.llm_factory import LLMFactory
 from src.openai.common.logger import init_logger
+
+import sqlite3
+from langgraph.checkpoint.sqlite import SqliteSaver
+
+# get to load environment variables
+load_dotenv(override=True)
+
+# create google search tool
+serper = GoogleSerperAPIWrapper()
+tool_search = Tool(
+    name="search",
+    func=serper.run,
+    description="Useful for when you need more information from an online search"
+)
+tools_list = [tool_search]
+
+# create llm
+open_ai_llm = LLMFactory.get_chat_open_ai_llm()
+llm_with_tools = open_ai_llm.bind_tools(tools_list)
+
+# create memory - only using internal memory object valid per python process
+llm_memory = MemorySaver()
+
+# use sql memory if you want
+db_path = "chatbot_memory.db"
+conn = sqlite3.connect(db_path, check_same_thread=False)
+sql_memory = SqliteSaver(conn)
+
+# flag what memory to use
+used_memory = sql_memory
 
 
 # //////////////// Step 1: Create state ////////////////
@@ -34,26 +66,31 @@ class ChatState(BaseModel):
 graph_builder = StateGraph(ChatState)
 
 
-# //////////////// Step 3: Create a Node ////////////////
+# //////////////// Step 3: Create a Nodes ////////////////
 
 def chatbot_node(old_state: ChatState) -> ChatState:
-    response = LLMFactory.get_chat_open_ai_llm().invoke(old_state.messages)
+    response = llm_with_tools.invoke(old_state.messages)
     new_state = ChatState(messages=[response])
     return new_state
 
 
 # add note into builder
 graph_builder.add_node("chatbot_node", chatbot_node)
+# consider tools as a node
+graph_builder.add_node("tools", ToolNode(tools=tools_list))
 
 # //////////////// Step 4: Create Edges ////////////////
 
+# conditionally run tools if needed
+graph_builder.add_conditional_edges("chatbot_node", tools_condition, "tools")
+# this will help to loop around
+graph_builder.add_edge("tools", "chatbot_node")
 graph_builder.add_edge(START, "chatbot_node")
-graph_builder.add_edge("chatbot_node", END)
 
 # //////////////// Step 5: Compile the Graph ////////////////
 
 # you can see that memory is added there
-graph = graph_builder.compile(checkpointer=MemorySaver())
+graph = graph_builder.compile(checkpointer=sql_memory)
 display(Image(graph.get_graph().draw_mermaid_png()))
 
 # //////////////// Step 6: Create simple Chat UI ////////////////
@@ -65,6 +102,9 @@ def start_session():
     """
     Create ID for conversation
     """
+    if isinstance(used_memory, SqliteSaver):
+        # use the same session ID each time in case of long term SQL memory
+        return 1
     return str(uuid.uuid4())
 
 
